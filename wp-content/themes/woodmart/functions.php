8807a54684eb65034457fc52baf180dfd6e28e78
Domagoj Muža <domim1998@gmail.com>
2026-02-27 22:11:53 +0100

    aaa

--- wp-content/themes/woodmart/functions.php
+++ wp-content/themes/woodmart/functions.php
@@ -57,12 +57,13 @@ define( 'WOODMART_VERSION', woodmart_get_theme_info( 'Version' ) );
 
 add_filter( 'woocommerce_get_availability', 'filter_product_get_availability', 10, 2);
 function filter_product_get_availability( $availability, $product ) {
-    if ( $product->get_stock_status() === 'instock' ) {
-        $availability['availability'] = wc_format_stock_for_display( $product );
-    } elseif ( $product->managing_stock() && $product->is_on_backorder( 1 ) 
-    && ! $product->backorders_require_notification() ) {
-        $availability['availability'] = __('In Stock', 'woocommerce');
-        $availability['class'] = 'in-stock';
-    }
+	echo $product->get_stock_status();
+    // if ( $product->get_stock_status() === 'instock' ) {
+    //     $availability['availability'] = wc_format_stock_for_display( $product );
+    // } elseif ( $product->managing_stock() && $product->is_on_backorder( 1 ) 
+    // && ! $product->backorders_require_notification() ) {
+    //     $availability['availability'] = __('In Stock', 'woocommerce');
+    //     $availability['class'] = 'in-stock';
+    // }
     return $availability;
 }
